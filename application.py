6273cf718e7f6cc4b1d573a2f938cce37897a30c
AhmadMajde22 <ahmadbara58.ad@gmail.com>
2025-06-21 00:56:50 +0300

    Add Data Drift Measure

--- application.py
+++ application.py
@@ -1,7 +1,16 @@
 import pickle
+
 import numpy as np
-import pandas as pd 
-from flask import Flask,render_template,request,jsonify
+import pandas as pd
+from alibi_detect.cd import KSDrift
+from flask import Flask, jsonify, render_template, request
+from sklearn.preprocessing import StandardScaler
+
+
+from src.feature_store import RedisFeatureStore
+from src.logger import get_logger
+
+logger = get_logger(__name__)
 
 app = Flask(__name__)
 
@@ -11,6 +20,26 @@ with open(MODEL_PATH,'rb') as model_file:
     model = pickle.load(model_file)
 
 FEATURE_NAMES = ['Age', 'Fare', 'Pclass', 'Sex', 'Embarked', 'FamilySize', 'IsAlone','HasCabin', 'Title', 'Pclass_Fare', 'Age_Fare']
+
+
+feature_store = RedisFeatureStore()
+scaler = StandardScaler()
+
+def fit_scaler_on_ref_data():
+    entity_ids = feature_store.get_all_entity_ids()
+    all_features = feature_store.get_batch_features(entity_ids)
+
+    all_features_df = pd.DataFrame.from_dict(all_features, orient='index', columns=FEATURE_NAMES)
+
+    scaler.fit(all_features_df)
+
+    return scaler.transform(all_features_df)
+
+historical_data = fit_scaler_on_ref_data()
+ksd = KSDrift(x_ref = historical_data,p_val =0.05)
+
+
+    
 
 @app.route("/")
 def home():
@@ -34,13 +63,24 @@ def predict():
 
         features = pd.DataFrame([[Age,Fare,Pclass,Sex,Embarked,FamilySize,IsAlone,HasCabin,Title,Pclass_Fare,Age_Fare]],columns=FEATURE_NAMES)
 
+        features_scaled = scaler.transform(features)
+
+        drift = ksd.predict(features_scaled)
+
+        drift_response = drift.get('data', {})
+        is_drift = drift_response.get('is_drift', None)
+
+        if is_drift is not None and is_drift:
+            print("Drift detected in the input features.")
+            logger.warning("Drift detected in the input features.")
+
         prediction = model.predict(features)[0]
 
         result = "Survived" if prediction == 1 else "Didn't Survive"
 
         return render_template("index.html", prediction=result)
     except Exception as e:
-        return render_template("index.html", prediction=f"Error: {str(e)}")
+        return jsonify("index.html", prediction=f"Error: {str(e)}")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name = "Titanic-Survaival",
-    version = "0.4",
+    version = "0.5",
     author = "Ahmad Majdi",
     packages = find_packages(),
     install_requires = requirements
